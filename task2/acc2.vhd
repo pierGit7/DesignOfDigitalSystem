ecdd034aeb662898d66e8395b71428b39a5aac84
pierfra _ <sorgiovanni60@gmail.com>
2024-11-04 14:39:14 +0100

    start Task2

--- task2/acc2.vhd
+++ task2/acc2.vhd
@@ -47,49 +47,45 @@ architecture rtl of acc is
 -- All internal signals are defined here
 
 type state_type is ( idle, invert, write, check_end);
+type state_type_t2 is (idle, read, computation);
+--type buffer_array is array(0 to 2, 0 to 2) of byte_t;
 
-signal state, next_state : state_type := idle;
+-- buffer of two rows
+type row_buffer_array is array(0 to 703) of byte_t;
+
+-- signal state, next_state : state_type := idle;
+-- signal computation_buffer : buffer_array := (others => (others => '0'));
+
+signal rows_buffer : row_buffer_array;
 signal reg, next_reg : halfword_t := halfword_zero;
+signal state_t2, next_state_t2 : state_type_t2 := idle;
+signal x_position, y_position : integer := 0;
+signal top_line_left, top_line_right : word_t := word_zero;
 
 begin
-    cl : process(start,state)
+    task2 : process(start, state_t2)
     begin
-        next_state <= state;
+        next_state_t2 <= state_t2;
         next_reg <= reg;
-        finish <= '0';
         addr <= reg;
-        
-        --addr <= halfword_zero;
-    
-        case (state) is
+
+        case (next_state_t2) is
             when idle =>
-                if start = '1' then
-                    en <= '1';
-                    next_state <= invert;
-                    --addr <= reg;
+                if start = '1' then 
+                    en <= '1'; 
+                    state_t2 <= read;                   
                 end if;
-            when invert =>
-                dataW <= not(dataR);
-                next_reg <= std_logic_vector(unsigned(reg) + 25344);
-                next_state <= write;
-            when write =>
-                we <= '1';
-                next_state <= check_end;
-            when check_end =>
-            -- 25343 means this was the last word to read
-                if (to_integer(unsigned(reg)) - 25344) /= 25343 then
-                    we <= '0';  
-                    next_reg <= std_logic_vector(unsigned(reg) - 25343);
+            when read =>
+                -- check if you read three rows
+                if y_position mod 3 = 0 then
+                    state_t2 <= computation;
                 else
-                    we <= '0';
-                    en <= '0';
-                    finish <= '1';
+                    y_position = y_position + 1;
+                    reg <= reg + (y_position * 352);
                 end if;
-                next_state<= idle;
-        end case;    
-        
-        
-    end process cl;
+            when computation => 
+        end case;
+    end process task2;
 
 -- Template for a process
     register_process : process(clk)
@@ -109,5 +105,38 @@ begin
             end if;
         end if;
     end process register_process;
-
+    
+    -- task0
+    -- cl : process(start,state)
+    -- begin
+    --     next_state <= state;
+    --     next_reg <= reg;
+    --     finish <= '0';
+    --     addr <= reg;
+    --     case (state) is
+    --         when idle =>
+    --             if start = '1' then
+    --                 en <= '1';
+    --                 next_state <= invert;
+    --             end if;
+    --         when invert =>
+    --             dataW <= not(dataR);
+    --             next_reg <= std_logic_vector(unsigned(reg) + 25344);
+    --             next_state <= write;
+    --         when write =>
+    --             we <= '1';
+    --             next_state <= check_end;
+    --         when check_end =>
+    --         -- 25343 means this was the last word to read
+    --             if (to_integer(unsigned(reg)) - 25344) /= 25343 then
+    --                 we <= '0';  
+    --                 next_reg <= std_logic_vector(unsigned(reg) - 25343);
+    --             else
+    --                 we <= '0';
+    --                 en <= '0';
+    --                 finish <= '1';
+    --             end if;
+    --             next_state<= idle;
+    --     end case;    
+    -- end process cl;
 end rtl;
