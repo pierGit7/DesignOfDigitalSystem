076fbaa51ea1663875ef508c78cd9e103b414194
pierfra _ <sorgiovanni60@gmail.com>
2024-11-07 11:59:56 +0100

    new implementation

--- task2/acc2.vhd
+++ task2/acc2.vhd
@@ -5,7 +5,7 @@
 --  Developers :  YOUR NAME HERE - s??????@student.dtu.dk
 --             :  YOUR NAME HERE - s??????@student.dtu.dk
 --             :
---  Purpose    :  This design contains an entity for the accelerator that must be build
+--  Purpose    :  This design contains an entity for the accelerator that must be built
 --             :  in task two of the Edge Detection design project. It contains an
 --             :  architecture skeleton for the entity as well.
 --             :
@@ -39,104 +39,103 @@ entity acc is
 end acc;
 
 --------------------------------------------------------------------------------
--- The desription of the accelerator.
+-- The description of the accelerator.
 --------------------------------------------------------------------------------
 
 architecture rtl of acc is
 
--- All internal signals are defined here
+    -- All internal signals are defined here
 
-type state_type is ( idle, invert, write, check_end);
-type state_type_t2 is (idle, read, computation);
---type buffer_array is array(0 to 2, 0 to 2) of byte_t;
+    type state_type_t2 is (idle, read, computation);
 
--- buffer of two rows
-type row_buffer_array is array(0 to 703) of byte_t;
+    -- Buffer of three rows
+    type row_buffer_array is array(0 to 87) of word_t;
+    type computation_register is std_logic_vector(0 to 47);
 
--- signal state, next_state : state_type := idle;
--- signal computation_buffer : buffer_array := (others => (others => '0'));
+    --TODO: init variables to 0
+    -- buffer three rows to acces three words each clock cycle
+    signal row1_buffer, row2_buffer, row3_buffer : row_buffer_array;
 
-signal rows_buffer : row_buffer_array;
-signal reg, next_reg : halfword_t := halfword_zero;
-signal state_t2, next_state_t2 : state_type_t2 := idle;
-signal x_position, y_position : integer := 0;
-signal top_line_left, top_line_right : word_t := word_zero;
+    signal comp1, comp2, comp3 : computation_register;
+    --TODO alias
+    
+    -- changing addres for main memory
+    signal reg, next_reg : halfword_t := halfword_zero;
+
+    --state of task2 process
+    signal state_t2, next_state_t2 : state_type_t2 := idle;
+
+    signal y_position : unsigned(10 downto 0) := (others => '0');  -- Explicit width
+    -- Start from pixel 1
+    signal x_position : unsigned(10 downto 0) := (others => '0');
+
+    signal index_of_buffer : unsigned(10 downto 0) := (others => '0');
 
 begin
     task2 : process(start, state_t2)
     begin
+        -- Default assignments to prevent latches
+        finish <= '0';
         next_state_t2 <= state_t2;
         next_reg <= reg;
         addr <= reg;
 
-        case (next_state_t2) is
+
+        case (state_t2) is
             when idle =>
                 if start = '1' then 
                     en <= '1'; 
-                    state_t2 <= read;                   
+                    next_state_t2 <= read;                   
                 end if;
+
             when read =>
-                -- check if you read three rows
-                if y_position mod 3 = 2 then
-                    state_t2 <= computation;
+                x_position = x_position + '1';
+                -- check wich row are we in
+                if x_position = 87  then
+                    index_of_buffer = index_of_buffer + '1';
+                    if index_of_buffer = 2 then
+                        index_of_buffer = '0';
+                    end if;
+                    y_position = y_position + '1';
+                    x_position = '0';
+                end if; 
+                if index_of_buffer = '0' then
+                    row1_buffer(x_position) = dataR;
+                elsif index_of_buffer = '1' then
+                    row2_buffer(x_position) = dataR;
                 else
-                    y_position = y_position + 1;
-                    reg <= reg + (y_position * 352);
+                    row3_buffer(x_position) = dataR;
                 end if;
-            when computation => 
+                next_reg <= std_logic_vector(unsigned(reg) + 1);
+            when computation =>
+                finish <= '1';  -- Signal the completion
+                next_state_t2 <= idle;  -- Go back to idle after computation is done
+
+            when others =>
+                next_state_t2 <= idle;
         end case;
     end process task2;
+    
+    
 
--- Template for a process
+    -- Register process
     register_process : process(clk)
     begin
         if rising_edge(clk) then
             if reset = '1' then
                 addr <= (others => '0');
-                state <= idle;
+                state_t2 <= idle;
                 reg <= (others => '0');
+                y_position <= (others => '0');  -- Initialize y_position
                 --en <= '0';
                 --we <= '0';
             else
                 -- Registers update
-                state <= next_state;
+                state_t2 <= next_state_t2;
                 reg <= next_reg;
                 addr <= next_reg;
             end if;
         end if;
     end process register_process;
-    
-    -- task0
-    -- cl : process(start,state)
-    -- begin
-    --     next_state <= state;
-    --     next_reg <= reg;
-    --     finish <= '0';
-    --     addr <= reg;
-    --     case (state) is
-    --         when idle =>
-    --             if start = '1' then
-    --                 en <= '1';
-    --                 next_state <= invert;
-    --             end if;
-    --         when invert =>
-    --             dataW <= not(dataR);
-    --             next_reg <= std_logic_vector(unsigned(reg) + 25344);
-    --             next_state <= write;
-    --         when write =>
-    --             we <= '1';
-    --             next_state <= check_end;
-    --         when check_end =>
-    --         -- 25343 means this was the last word to read
-    --             if (to_integer(unsigned(reg)) - 25344) /= 25343 then
-    --                 we <= '0';  
-    --                 next_reg <= std_logic_vector(unsigned(reg) - 25343);
-    --             else
-    --                 we <= '0';
-    --                 en <= '0';
-    --                 finish <= '1';
-    --             end if;
-    --             next_state<= idle;
-    --     end case;    
-    -- end process cl;
+
 end rtl;
