e68009b9ee37c5d43631729278888e8a6fcd7a74
HakunV <hakun_vestergaard@hotmail.com>
2024-11-11 17:04:39 +0100

    Commit Fraud

--- task2/acc2.vhd
+++ task2/acc2.vhd
@@ -46,33 +46,39 @@ architecture rtl of acc is
 
     -- All internal signals are defined here
 
-    type state_type_t2 is (idle, read, computation);
+    type state_type is (idle, read, computation);
 
     -- Buffer of three rows
-    type row_buffer_array is array(0 to 87) of word_t;
+    type row_buffer_array is array(87 to 0) of word_t;
     --type computation_register is std_logic_vector(0 downto 47);
 
     --TODO: init variables to 0
     -- buffer three rows to acces three words each clock cycle
     signal row1_buffer, row2_buffer, row3_buffer : row_buffer_array;
 
-    signal comp1, comp2, comp3 : std_logic_vector(0 downto 47);
+    signal comp1, comp2, comp3 : std_logic_vector(47 downto 0);
     --TODO alias
     
     -- changing addres for main memory
     signal reg, next_reg : halfword_t := halfword_zero;
 
     --state of task2 process
-    signal state_t2, next_state_t2 : state_type_t2 := idle;
+    signal state, next_state : state_type := idle;
 
     signal y_position : integer := 0;  -- Explicit width
     -- Start from pixel 1
     signal x_position : integer := 0;
 
     signal index_of_buffer : integer := 0;
+    
+    signal pixel_out : std_logic_vector(39 downto 0);
+    
+    signal output_flag : boolean := false;
+    
+    signal Dx_1, Dy_1, Dx_2, Dy_2 : signed(63 downto 0);
 
 begin
-    task2 : process(start, state_t2)
+    task2 : process(start, state)
     begin
         -- Default assignments to prevent latches
         finish <= '0';
@@ -108,8 +114,26 @@ begin
                 end if;
                 next_reg <= std_logic_vector(unsigned(reg) + 1);
             when computation =>
-                finish <= '1';  -- Signal the completion
-                next_state_t2 <= idle;  -- Go back to idle after computation is done
+                Dx_1 <= signed(unsigned(comp1(23 downto 16)) - unsigned(comp1(7 downto 0)) + 2*(unsigned(comp2(23 downto 16)) - unsigned(comp2(7 downto 0))) + unsigned(comp3(23 downto 16)) - unsigned(comp3(7 downto 0)));
+                Dy_1 <= signed(unsigned(comp1(7 downto 0)) - unsigned(comp3(7 downto 0)) + 2*(unsigned(comp1(15 downto 8)) - unsigned(comp3(15 downto 8))) + unsigned(comp1(23 downto 16)) - unsigned(comp3(23 downto 16)));
+                
+                Dx_2 <= signed(unsigned(comp1(31 downto 24)) - unsigned(comp1(7 downto 0)) + 2*(unsigned(comp2(23 downto 16)) - unsigned(comp2(7 downto 0))) + unsigned(comp3(23 downto 16)) - unsigned(comp3(7 downto 0)));
+                Dx_2 <= ;
+                
+                if output_flag = false then
+                    pixel_out(15 downto 8) <= std_logic_vector(abs(Dx_1) + abs(Dy_1));
+                    pixel_out(23 downto 16) <= std_logic_vector(abs(Dx_2) + abs(Dy_2));
+                    output_flag <= true;
+                else
+                    pixel_out(31 downto 24) <= std_logic_vector(abs(Dx_1) + abs(Dy_1));
+                    pixel_out(39 downto 32) <= std_logic_vector(abs(Dx_2) + abs(Dy_2));
+                    output_flag <= false;
+                    next_state <= write;
+                end if;
+
+
+                next_reg <= std_logic_vector(unsigned(reg) + 25343);
+                
             when others =>
                 next_state_t2 <= idle;
         end case;
