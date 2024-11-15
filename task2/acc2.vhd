c3922a111ba147e9aefc75e4d409c007c9f00f95
Simon Craig <simonthomascraig@outlook.com>
2024-11-15 16:43:47 +0100

    Commit treason 2

--- task2/acc2.vhd
+++ task2/acc2.vhd
@@ -56,16 +56,6 @@ architecture rtl of acc is
     -- buffer three rows to acces three words each clock cycle
     signal row1_buffer, row2_buffer, row3_buffer : row_buffer_array := (others=>(others => '0'));
 
-<<<<<<< Updated upstream
-    signal comp1, comp2, comp3 : std_logic_vector(0 downto 47);
-    --TODO alias
-    
-    -- changing addres for main memory
-    signal reg, next_reg : halfword_t := halfword_zero;
-
-    --state of task2 process
-    signal state_t2, next_state_t2 : state_type_t2 := idle;
-=======
     -- computation registers
     signal comp1, comp2, comp3 : std_logic_vector(47 downto 0);
     --TODO alias
@@ -75,15 +65,11 @@ architecture rtl of acc is
 
     --state of task2 process
     signal state, next_state : state_type := idle;
->>>>>>> Stashed changes
 
-    signal y_position : integer := 0;  -- Explicit width
+    signal y_position, next_y_position : integer := 0;  -- Explicit width
     -- Start from pixel 1
-    signal x_position : integer := 0;
+    signal x_position, next_x_position : integer := 0;
 
-<<<<<<< Updated upstream
-    signal index_of_buffer : integer := 0;
-=======
     signal index_of_buffer, index_of_load, next_index_of_load : integer := 0;
     
     signal output_flag : boolean := true;
@@ -91,39 +77,20 @@ architecture rtl of acc is
     signal EOL_flag : boolean := false;
     
     signal pixel_out, next_pixel_out, pixel_temp : std_logic_vector(39 downto 0);
->>>>>>> Stashed changes
 
 begin
-    task2 : process(start, state_t2)
+    task2 : process(start, state)
     begin
         -- Default assignments to prevent latches
         finish <= '0';
-<<<<<<< Updated upstream
-        next_state_t2 <= state_t2;
-        next_reg <= reg;
-        addr <= reg;
-
-
-        case (state_t2) is
-=======
         next_state <= state;
         --addr <= next_reg;
         next_reg <= reg;
         case (state) is
->>>>>>> Stashed changes
             when idle =>
                 if start = '1' then 
                     we <= '0';
                     en <= '1'; 
-<<<<<<< Updated upstream
-                    next_state_t2 <= read;                   
-                end if;
-
-            when read =>
-                x_position <= x_position + 1;
-                -- check wich row are we in
-                if x_position = 87  then
-=======
                     next_state <= initialise;                   
                 end if;
 			when initialise =>
@@ -133,22 +100,15 @@ begin
                 -- check wich row are we in
                 if x_position = 87  then
                     EOL_flag <= true;
->>>>>>> Stashed changes
                     index_of_buffer <= index_of_buffer + 1;
                     if index_of_buffer = 2 then
                         index_of_buffer <= 0;
                     end if;
-<<<<<<< Updated upstream
-                    y_position <= y_position + 1;
-                    x_position <= 0;
-                end if; 
-=======
                     next_y_position <= y_position + 1;
                     next_x_position <= 0;
                 else
                     EOL_flag <= false;
                 end if;
->>>>>>> Stashed changes
                 if index_of_buffer = 0 then
                     row1_buffer(x_position) <= dataR;
                 elsif index_of_buffer = 1 then
@@ -173,19 +133,6 @@ begin
 						next_state <= read;
 				    end if;
                 end if;
-<<<<<<< Updated upstream
-                next_reg <= std_logic_vector(unsigned(reg) + 1);
-            when computation =>
-                finish <= '1';  -- Signal the completion
-                next_state_t2 <= write;  -- Go back to idle after computation is done
-            when write =>
-                
-                next_state_t2 <= check_finish;
-                next_reg <= std_logic_vector(unsigned(reg) - 25343);
-            when check_finish =>
-                next_state_t2 <= idle;
-               
-=======
             when read =>
                 next_x_position <= x_position + 1;
 				next_reg <= write_reg;
@@ -252,9 +199,8 @@ begin
 				    finish <= '1';
 				    next_state <= idle;
 				end if;
->>>>>>> Stashed changes
             when others =>
-                next_state_t2 <= idle;
+                next_state <= idle;
         end case;
     end process task2;
     
@@ -274,6 +220,7 @@ begin
                 next_pixel_out(15 downto 8) <= std_logic_vector(abs(signed(unsigned(comp1(23 downto 16)) - unsigned(comp1(7 downto 0)) + 2*(unsigned(comp2(23 downto 16)) - unsigned(comp2(7 downto 0))) + unsigned(comp3(23 downto 16)) - unsigned(comp3(7 downto 0)))) + abs(signed(unsigned(comp1(7 downto 0)) - unsigned(comp3(7 downto 0)) + 2*(unsigned(comp1(15 downto 8)) - unsigned(comp3(15 downto 8))) + unsigned(comp1(23 downto 16)) - unsigned(comp3(23 downto 16)))));
                 next_pixel_out(7 downto 0) <= std_logic_vector(abs(signed(unsigned(comp1(31 downto 24)) - unsigned(comp1(15 downto 8)) + 2*(unsigned(comp2(31 downto 24)) - unsigned(comp2(15 downto 8))) + unsigned(comp3(31 downto 24)) - unsigned(comp3(15 downto 8)))) + abs(signed(unsigned(comp1(15 downto 8)) - unsigned(comp3(31 downto 24)) + 2*(unsigned(comp1(23 downto 16)) - unsigned(comp3(23 downto 16))) + unsigned(comp1(31 downto 24)) - unsigned(comp3(31 downto 24)))));
                
+                
             end if;
          end if;
     end process compute_process;
@@ -284,18 +231,12 @@ begin
         if rising_edge(clk) then
             if reset = '1' then
                 addr <= (others => '0');
-                state_t2 <= idle;
+                state <= idle;
                 reg <= (others => '0');
-<<<<<<< Updated upstream
-                y_position <= 0;  -- Initialize y_position
-                --en <= '0';
-                --we <= '0';
-=======
                 y_position <= 0; 
->>>>>>> Stashed changes
             else
                 -- Registers update
-                state_t2 <= next_state_t2;
+                state <= next_state;
                 reg <= next_reg;
                 write_reg <= next_write_reg;
                 read_reg <= next_read_reg;
